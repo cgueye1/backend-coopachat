64009c1d0a9c370e22bb395c979992c2514bf2e1
sdfaye <sdfaye@innovimpactafrica.com>
2026-03-12 10:31:54 +0100

    Mise à jour des infos du livreur affichés sur l'interface du client

--- src/main/java/com/example/coopachat/dtos/order/ClientOrderItemDTO.java
+++ src/main/java/com/example/coopachat/dtos/order/ClientOrderItemDTO.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientOrderItemDTO {
-    private Long productId;
+    private Long OrderItemId;
     private String productName;
     private Integer quantity;
     private BigDecimal unitPrice;
