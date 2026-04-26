0c3e4d9272b5fc35d20f6f94b8b6d5ea3bc0dda2
sdfaye <sdfaye@innovimpactafrica.com>
2026-04-26 23:24:45 +0100

    Nouvelle Modification

--- src/main/java/com/example/coopachat/dtos/Payment/PaymentBridgeResponseDTO.java
+++ src/main/java/com/example/coopachat/dtos/Payment/PaymentBridgeResponseDTO.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentBridgeResponseDTO {
+    private String merchantToken;
     private String transactionReference;
     private String agencyCode;
     private String serviceId;
